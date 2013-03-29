2035ec58068ae95a781dae803dd9eba8a1333a3b
Edison <hydra1983@gmail.com>
2013-03-29 13:11:36 +0800

    add StartsWith and EndsWith conditions.

---
+++ src/main/java/net/sf/antcontrib/logic/condition/EndsWith.java
@@ -0,0 +1,37 @@
+package net.sf.antcontrib.logic.condition;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.condition.IsTrue;
+
+public class EndsWith extends IsTrue {
+
+	private String string = null;
+	private String propertyName = null;
+	private String with = null;
+
+	public void setString(String s) {
+		this.string = s;
+	}
+
+	public void setProperty(String s) {
+		this.propertyName = s;
+	}
+
+	public void setWith(String w) {
+		this.with = getProject().replaceProperties(w);
+	}
+
+	public boolean eval() throws BuildException {
+		if (string == null || propertyName == null)
+			throw new BuildException("string or property must be set.");
+		if (with == null)
+			throw new BuildException("with must be set.");
+
+		if (string != null)
+			return string.endsWith(with);
+		else {
+			String propertyValue = getProject().getProperty(propertyName);
+			return propertyValue == null ? false : propertyValue.endsWith(with);
+		}
+	}
+}
---
+++ src/main/java/net/sf/antcontrib/logic/condition/StartsWith.java
@@ -0,0 +1,38 @@
+package net.sf.antcontrib.logic.condition;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.condition.IsTrue;
+
+public class StartsWith extends IsTrue {
+
+	private String string = null;
+	private String propertyName = null;
+	private String with = null;
+
+	public void setString(String s) {
+		this.string = s;
+	}
+
+	public void setProperty(String s) {
+		this.propertyName = s;
+	}
+
+	public void setWith(String w) {
+		this.with = getProject().replaceProperties(w);
+	}
+
+	public boolean eval() throws BuildException {
+		if (string == null || propertyName == null)
+			throw new BuildException("string or property must be set.");
+		if (with == null)
+			throw new BuildException("with must be set.");
+
+		if (string != null)
+			return string.startsWith(with);
+		else {
+			String propertyValue = getProject().getProperty(propertyName);
+			return propertyValue == null ? false : propertyValue
+					.startsWith(with);
+		}
+	}
+}
